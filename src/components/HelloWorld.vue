4ae0ebc6de2f08294a70d7ee11bf7c238603151c
wangchunsheng <chunsheng.wang@ys100.com>
2020-11-24 17:35:08 +0800

    wcs 按需加载提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,115 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="shuttle">
+    <div class="box">
+      <div class="left">
+        <el-tree
+          :props="props"
+          :load="loadNode"
+          lazy
+          show-checkbox
+          @check-change="handleCheckChange"
+        >
+        </el-tree>
+      </div>
+      <div class="right">
+        <ul>
+          <li v-for="item in nodeList" :key="item.id">
+            <p>{{ item.label }}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <el-button @click="submit" size="small" type="primary">主要按钮</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "Shuttle",
+  data() {
+    return {
+      props: {
+        label: "name",
+        children: "zones",
+      },
+      count: 1,
+      nodeList: [],
+    };
+  },
+  methods: {
+    handleCheckChange(data, checked, indeterminate) {
+      console.log(data, checked, indeterminate);
+    },
+
+    loadNode(node, resolve) {
+      if (node.level === 0) {
+        return resolve([{ name: "region1" }, { name: "region2" }]);
+      }
+      if (node.level > 3) return resolve([]);
+
+      var hasChild;
+      if (node.data.name === "region1") {
+        hasChild = true;
+      } else if (node.data.name === "region2") {
+        hasChild = false;
+      } else {
+        hasChild = Math.random() > 0.5;
+      }
+
+      setTimeout(() => {
+        var data;
+        if (hasChild) {
+          data = [
+            {
+              name: "zone" + this.count++,
+            },
+            {
+              name: "zone" + this.count++,
+            },
+          ];
+        } else {
+          data = [];
+        }
+
+        resolve(data);
+      }, 500);
+    },
+    handleNodeClick() {
+      // let res = this.$refs.tree.getCheckedNodes();
+      // let arr = [];
+      // console.log(res);
+      // res.forEach((item) => {
+      //   let a = {};
+      //   a.id = item.id;
+      //   a.label = item.label;
+      //   arr.push(a);
+      // });
+      // this.nodeList = arr;
+      // console.log(
+      //   this.$refs.tree
+      //     .getCheckedKeys()
+      //     .concat(this.$refs.tree.getHalfCheckedKeys())
+      // );
+    },
+    submit() {
+      alert(JSON.stringify(this.nodeList));
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.box {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 500px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.left,
+.right {
+  height: 500px;
+  width: 240px;
+  border: 1px solid #ccc;
 }
 </style>
